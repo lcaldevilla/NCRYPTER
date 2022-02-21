a9c1c1e9821926669d42e447db4096208f64ba3e
Luis Caldevilla <luiscaldevilla@gmail.com>
2022-02-21 08:09:53 +0100

    Commit inicial

---
+++ CifrarDescifrar/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CifrarDescifrar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = cifrar(textBox1.Text);
+        }
+
+        public string cifrar(string cadena)
+        {
+            string clave = "cadenadecifrado";
+            byte[] llave; //Arreglo donde guardaremos la llave para el cifrado 3DES.
+
+            byte[] arreglo = UTF8Encoding.UTF8.GetBytes(cadena); //Arreglo donde guardaremos la cadena descifrada.
+
+            // Ciframos utilizando el Algoritmo MD5.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(clave));
+            md5.Clear();
+
+            //Ciframos utilizando el Algoritmo 3DES.
+            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider();
+            tripledes.Key = llave;
+            tripledes.Mode = CipherMode.ECB;
+            tripledes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform convertir = tripledes.CreateEncryptor(); // Iniciamos la conversión de la cadena
+            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length); //Arreglo de bytes donde guardaremos la cadena cifrada.
+            tripledes.Clear();
+
+            return Convert.ToBase64String(resultado, 0, resultado.Length); // Convertimos la cadena y la regresamos.
+        }
+
+        public string descifrar(string cadena)
+        {
+            string clave = "cadenadecifrado";
+            byte[] llave;
+
+            byte[] arreglo = Convert.FromBase64String(cadena); // Arreglo donde guardaremos la cadena descovertida.
+            
+
+            // Ciframos utilizando el Algoritmo MD5.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            llave = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(clave));
+            md5.Clear();
+
+            //Ciframos utilizando el Algoritmo 3DES.
+            TripleDESCryptoServiceProvider tripledes = new TripleDESCryptoServiceProvider();
+            tripledes.Key = llave;
+            tripledes.Mode = CipherMode.ECB;
+            tripledes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform convertir = tripledes.CreateDecryptor();
+            byte[] resultado = convertir.TransformFinalBlock(arreglo, 0, arreglo.Length);
+            tripledes.Clear();
+
+            string cadena_descifrada = UTF8Encoding.UTF8.GetString(resultado); // Obtenemos la cadena
+            return cadena_descifrada; // Devolvemos la cadena
+        }
+
+        /// <summary>
+        /// Lo usamos para desencriptar la clave del usuario administrador local
+        /// </summary>
+        /// <param name="szPlainText"></param>
+        /// <param name="szEncryptionKey"></param>
+        /// <returns></returns>
+        public string EncryptDecrypt(string szPlainText, int szEncryptionKey)
+        {
+            StringBuilder szInputStringBuild = new StringBuilder(szPlainText);
+            StringBuilder szOutStringBuild = new StringBuilder(szPlainText.Length);
+            char Textch;
+            for (int iCount = 0; iCount < szPlainText.Length; iCount++)
+            {
+                Textch = szInputStringBuild[iCount];
+                Textch = (char)(Textch ^ szEncryptionKey);
+                szOutStringBuild.Append(Textch);
+            }
+            
+            return szOutStringBuild.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = descifrar(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           textBox1.Text= EncryptDecrypt(textBox2.Text, 200);
+        }
+    }
+}
